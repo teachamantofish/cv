acc06486448c13ea5e7130c51c18fe38efa87cdb
teachamantofish <aardvarkman@gmail.com>
2022-09-22 12:23:30 -0400

    updates

--- rst/conf.py
+++ rst/conf.py
@@ -38,7 +38,7 @@ html_context = {
 # further.  For a list of options available for each theme, see the documentation.
 
 html_theme_options = {
-    'canonical_url': 'https://www.linkedin.com/in/ben-rogers-77a625a/',
+    'canonical_url': 'https://teachamantofish.github.io/cv/',
     'logo_only': False,
     'display_version': False,
     'prev_next_buttons_location': 'top',
